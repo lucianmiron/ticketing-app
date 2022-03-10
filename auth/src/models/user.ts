ca15cb70c8445f1cbe6b70b4ba9c43688fd58dc1
mironlucian <mironlucianv@gmail.com>
2022-03-10 20:22:35 +0200

    mongoose with user model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+// An interface that describes the properties
+// that are requried to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>('User', userSchema);
+
+export { User };
