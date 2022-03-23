2fcb5a9e4b784c66649da9e3dca0bd7182493c83
mironlucian <mironlucianv@gmail.com>
2022-03-23 20:19:35 +0200

    server side render, auth complete

---
+++ client/api/build-client.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export default ({ req }) => {
+  if (typeof window === 'undefined') {
+    // server side
+    return axios.create({
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers
+    })
+  } else {
+    // browser/client side
+    return axios.create({
+      baseURL: '/'
+    })
+  }
+}
