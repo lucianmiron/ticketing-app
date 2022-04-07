b4bf660c356bf0733d3fa2ba8274da9064234385
mironlucian <mironlucianv@gmail.com>
2022-04-07 21:10:51 +0300

    Unit test (not working), Nats streaming service

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ticketing-app
+# Installation
+
+# Development
+
+Start up listening to code changes and re-build the app:
+
+```console
+skaffold dev
+```
+
+Publish the common npm library:
+
+```console
+npm run pub
+```
+
+### Usefull cmds
---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,16 @@
+import { Message } from 'node-nats-streaming';
+import { Listener } from './base-listener';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  // readonly is required by typescript in order to ensure that it's type will note be changed in the following code
+  readonly subject = Subjects.TicketCreated;
+  queueGroupName = 'payments-service';
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
+    console.log('Event data:', data);
+
+    msg.ack();
+  }
+}
